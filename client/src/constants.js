d352c1a6ad230d1e274de6b44a0ccdda71a7f521
130602 <harshita.upadhyay@students.iiit.ac.in>
2021-07-11 20:11:04 +0530

    backend

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
 export const APP_NAME = 'ENCALL';
 //export const repoName = '';
 export const APP_URL = 'https://encall.netlify.app';
-export const BACKEND_URL = 'https://arcane-atoll-99118.herokuapp.com/';
+export const BACKEND_URL = 'https://encall.azurewebsites.net/';
 // export const BACKEND_URL='http://localhost:5000';
